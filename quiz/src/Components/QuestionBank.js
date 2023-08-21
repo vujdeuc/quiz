3c73fb33c71bd9e36f471ea02833dc8fa78f2066
vujdeuc <vujdeucatalin@gmail.com>
2023-08-21 18:51:45 +0300

    Initial commit

---
+++ quiz/src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the Great Red Spot on Jupiter?",
+		options: ["a volcano", "a crater", "a lake", "a storm"],
+		answer: "a storm",
+	},
+	{
+		id: 2,
+		question: "The largest volcano in the solar system is called Olympus Mons. Where is it?",
+		options: ["Mars", "Earth", "Jupiter", "Saturn"],
+		answer: "Mars",
+	},
+	{
+		id: 3,
+		question: "Which of this planets is the smallest?",
+		options: ["Earth", "Mercury", "Jupiter", "Uranus"],
+		answer: "Mercury"
+	},
+	{
+		id: 4,
+		question: "Which is the closest planet to the Sun?",
+		options: ["Venus", "Earth", "Mercury", "Mars"],
+		answer: "Mercury"
+	},
+	{
+		id: 5,
+		question: "How many moons does Mars have?",
+		options: ["2", "1", "0", "5"],
+		answer: "2"
+	},
+]
+
+export default qBank;
